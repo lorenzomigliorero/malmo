0938122ecf3c7d10e50ea65e401b1ee01708246b
Lorenzo Migliorero <lorenzo.migliorero@aquest.it>
2018-11-21 12:18:20 +0100

    test(update-package): add basic spec
    affects: @malmo/cli

--- packages/cli/src/scripts/update-package.js
+++ packages/cli/src/scripts/update-package.js
@@ -4,10 +4,11 @@ const { kebabCase } = require('lodash');
 module.exports = ({
   name,
   description,
+  packagePath = `${process.env.PWD}/package.json`,
 }) => {
-  const mainPackage = editJsonFile(`${process.env.PWD}/package.json`);
+  const mainPackage = editJsonFile(packagePath);
   mainPackage.set('name', kebabCase(name));
   if (description) mainPackage.set('description', description);
-  mainPackage.set('version', '0.0.0');
+  mainPackage.set('version', '1.0.0');
   mainPackage.save();
 };
---
+++ packages/cli/test/update-package.spec.js
@@ -0,0 +1,37 @@
+const updatePackage = require('../src/scripts/update-package');
+
+jest.mock('fs', () => new (require('memfs').Volume)());
+
+beforeEach(() => {
+  require('fs').writeFileSync('/mocked-package.json', JSON.stringify({
+    name: 'bar',
+    description: 'foo',
+  }));
+});
+
+describe('update package', () => {
+  it('basic', () => {
+    updatePackage({
+      name: 'foo',
+      packagePath: '/mocked-package.json',
+    });
+    const pkg = JSON.parse(require('fs').readFileSync('/mocked-package.json').toString());
+    expect(pkg).toEqual(expect.objectContaining({
+      name: 'foo',
+      description: 'foo',
+    }));
+  });
+
+  it('with description param', () => {
+    updatePackage({
+      name: 'foo',
+      description: 'bar',
+      packagePath: '/mocked-package.json',
+    });
+    const pkg = JSON.parse(require('fs').readFileSync('/mocked-package.json').toString());
+    expect(pkg).toEqual(expect.objectContaining({
+      name: 'foo',
+      description: 'bar',
+    }));
+  });
+});
