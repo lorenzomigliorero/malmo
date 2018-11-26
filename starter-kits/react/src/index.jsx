25316cf0dbf01fd281524eef01c9109b7fae328e
Lorenzo Migliorero <lorenzo.migliorero@aquest.it>
2018-11-26 23:28:22 +0100

    refactor(welcome): replace old JSX with Welcome Component
    affects: @malmo/react-starter-kit

--- starter-kits/react/src/index.jsx
+++ starter-kits/react/src/index.jsx
@@ -1,49 +1,16 @@
-import React, { Fragment } from 'react';
+import React from 'react';
+import Welcome from '@malmo/welcome-react';
 import { render } from 'react-dom';
 import '@/styles.scss';
 import logo from '@/assets/logo.svg';
-import pkg from '../package.json';
+import { dependencies } from '../package.json';
 
 const renderComponent = () => (
-  <Fragment>
-    <img
-      alt=""
-      width={250}
-      src={logo}
-    />
-    <br />
-    <h1>
-      React Starter Kit&nbsp;
-      {pkg.version}
-    </h1>
-    <p>
-      {pkg.description}
-    </p>
-    <h2>
-      Main dependencies:
-    </h2>
-    <pre>
-      {JSON.stringify(pkg.dependencies, null, 4)}
-    </pre>
-    <h2>
-      Launch development server:
-    </h2>
-    <pre>
-      malmo dev
-    </pre>
-    <h2>
-      Build project:
-    </h2>
-    <pre>
-      malmo build
-    </pre>
-    <h2>
-      Build project using custom env:
-    </h2>
-    <pre>
-      malmo build --env=staging
-    </pre>
-  </Fragment>
+  <Welcome
+    logo={logo}
+    name="React Starter Kit"
+    dependencies={dependencies}
+  />
 );
 
 render(renderComponent(), global.document.getElementById('root'));
