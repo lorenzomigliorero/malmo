afe3ddd615b6d6fae0152b2ec95c0662b18b5b28
Lorenzo Migliorero <lorenzomigliorero@gmail.com>
2019-01-25 16:40:42 +0100

    fix(css-modules): remove module from Welcome React Component
    affects: @malmo/welcome-react, @malmo/welcome-styles

--- welcome/react/index.jsx
+++ welcome/react/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styles from '@malmo/welcome-styles';
+import '@malmo/welcome-styles';
 
 const Welcome = ({
   logo,
@@ -8,10 +8,10 @@ const Welcome = ({
   dependencies,
   children,
 }) => (
-  <section className={styles.wrapper}>
-    <div className={styles.header}>
+  <section className="welcome__wrapper">
+    <div className="welcome__header">
       <img
-        className={styles.logo}
+        className="welcome__logo"
         alt=""
         width={300}
         src={logo}
@@ -21,32 +21,32 @@ const Welcome = ({
       </h1>
     </div>
     {children && (
-      <div className={`${styles.children} ${styles.boxed}`}>
+      <div className="welcome__children welcome__boxed">
         {children}
       </div>
     )}
     <h2>
       Main dependencies:
     </h2>
-    <pre className={styles.boxed}>
+    <pre className="welcome__boxed">
       {JSON.stringify(dependencies, null, 4)}
     </pre>
     <h2>
       Launch development server:
     </h2>
-    <pre className={styles.boxed}>
+    <pre className="welcome__boxed">
       malmo dev
     </pre>
     <h2>
       Build project:
     </h2>
-    <pre className={styles.boxed}>
+    <pre className="welcome__boxed">
       malmo build
     </pre>
     <h2>
       Build project using custom env:
     </h2>
-    <pre className={styles.boxed}>
+    <pre className="welcome__boxed">
       malmo build --env=staging
     </pre>
   </section>
