6c2d520c57a844f3d24beb2fb582d7ed54abb2e0
Lorenzo Migliorero <lorenzomigliorero@gmail.com>
2019-07-15 11:48:23 +0200

    fix(webpack-hot-middleware): add path for __webpack_hmr scripts
    affects: malmo
    
    Add publicPath as path parameter in webpack-hot-middleware/client script for allow external usage

--- packages/cli/src/config/client.hmr.js
+++ packages/cli/src/config/client.hmr.js
@@ -3,15 +3,10 @@ const merge = require('webpack-merge');
 const clientConfig = require('./client');
 
 module.exports = () => {
+  const { publicPath } = require('../constants');
   const config = merge(clientConfig(), {
-    entry: {
-      main: [
-        'webpack-hot-middleware/client',
-      ],
-    },
-    plugins: [
-      new webpack.HotModuleReplacementPlugin(),
-    ],
+    entry: { main: [`webpack-hot-middleware/client?path=${publicPath}__webpack_hmr`] },
+    plugins: [new webpack.HotModuleReplacementPlugin()],
   });
 
   return config;
